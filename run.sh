4e67a1fdfd2e0a7621f7c66b204f499d4a569397
Lorenzo Giusti <giusti@diag.uniroma1.it>
2022-04-27 21:18:44 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,9 +4,9 @@ do
 	do
 		for att in 'T' 'F'
 		do 
-			for exp_num in {3..6}
+			for exp_num in {0..9}
 			do
-				s=$(printf "\-att %c -id 4 -e 4 --features \"[256,256,256,1]\" -lr 0.1 -wd 0.1 -k 2 -a relu -ns 0.1 --pct_miss %d --order %d --exp_num %d" $att $pct_miss $order $exp_num)
+				s=$(printf "\-att %c -id 0 -e 4 --features \"[256,256,256,1]\" -lr 0.1 -wd 0.1 -k 2 -a relu -ns 0.1 --pct_miss %d --order %d --exp_num %d" $att $pct_miss $order $exp_num)
 				s="${s:1}"
 				a=$(date)
 				echo $s > "$a".txt
